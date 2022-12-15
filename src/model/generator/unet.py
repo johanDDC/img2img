600fb022f6bb7f931ff049a714559202b121110b
johanDDC <ivan,peshekhonov@gmail.com>
2022-12-15 05:00:26 +0300

    Done unet

--- src/model/generator/unet.py
+++ src/model/generator/unet.py
@@ -1,45 +1,30 @@
 import torch
 import torch.nn as nn
+import torch.nn.init as init
 
-from src.model.generator.unet_block import DownsamplingUnetBlock, UpsamplingUnetBlock
-
+from src.model.generator.decoder import Decoder
+from src.model.generator.encoder import Encoder
 
 
 class Unet(nn.Module):
-    def __init__(self, img_shape, n_layers, in_channels, out_channels,
-                 dropout_prob=0.3):
+    def __init__(self, n_layers, in_channels, inner_channels, out_channels, start_num_filters):
         super().__init__()
         self.n_layers = n_layers
-        self.down_blocks = nn.ModuleList()
-        self.up_blocks = nn.ModuleList()
+        self.encoder = Encoder(n_layers, in_channels, inner_channels, start_num_filters)
+        self.decoder = Decoder(n_layers - 1, inner_channels, start_num_filters)
+        self.head = nn.Sequential(
+            nn.ReLU(),
+            nn.ConvTranspose2d(2 * start_num_filters, out_channels, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1)),
+            nn.Tanh()
+        )
 
-        h, w = img_shape
-        n_channels = in_channels
-        for layer in range(self.n_layers):
-            self.down_blocks.append(
-                DownsamplingUnetBlock((n_channels, w, h), n_channels, out_channels, dropout_prob)
-            )
-            self.up_blocks.append(
-                UpsamplingUnetBlock((out_channels * 2, w, h), out_channels * 2, n_channels, dropout_prob)
-            )
-            n_channels = out_channels
-            out_channels *= 2
-            h, w = h // 2, w // 2
-
-        self.bottleneck = DownsamplingUnetBlock((n_channels, w, h), n_channels, out_channels, dropout_prob)
+    @staticmethod
+    def __init_weights(layer):
+        if isinstance(layer, (nn.Conv2d, nn.ConvTranspose2d)):
+            init.normal_(layer.weight, 0, 0.02)
+            init.constant_(layer.bias, 0)
 
     def forward(self, img):
-        residuals = []
-        output = img
-        for layer in range(self.n_layers):
-            residual, output = self.down_blocks[layer](output)
-            residuals.append(residual)
-        output, _ = self.bottleneck(output)
-        for layer in range(self.n_layers):
-            pass
-
-
-
-
-
-
+        encoder_out, residuals = self.encoder(img)
+        result = self.decoder(encoder_out, residuals)
+        return self.head(result)
