d21b9f9465a9a7a618902a121bd0699093fd2c04
johanDDC <ivan,peshekhonov@gmail.com>
2022-12-15 07:49:12 +0300

    Implement train loop

---
+++ data/download_tiny_imagenet.sh
@@ -0,0 +1,2 @@
+wget https://www.dropbox.com/s/33l8lp62rmvtx40/dataset.zip -O dataset.zip
+unzip -q dataset.zip
---
+++ utils.py
@@ -0,0 +1,12 @@
+import random
+import torch
+
+import numpy as np
+
+
+def set_random_seed(seed):
+    torch.backends.cudnn.deterministic = True
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
